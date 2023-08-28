b94b1b38d5bd458b84cb2ce8e179054de0822c79
The Stupid Programmer <thestupidprogrammer@gmail.com>
2023-08-28 16:33:34 +0100

    changing path for custom domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://stupid-programmer.github.io',
-	base: '/blog',
+	site: 'https://thestupidprogrammer.com',
 	integrations: [mdx(), sitemap()],
 });
